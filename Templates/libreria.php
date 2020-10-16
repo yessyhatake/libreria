0f33a43be655f38a4499aea75d91ce4e899192f8
yessyhatake <72254124+yessyhatake@users.noreply.github.com>
2020-10-15 23:07:31 -0500

    Add files via upload

---
+++ Templates/libreria.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<!-- TemplateBeginEditable name="doctitle" -->
+<title>Documento sin título</title>
+<!-- TemplateEndEditable -->
+<!-- TemplateBeginEditable name="head" -->
+<!-- TemplateEndEditable -->
+</head>
+<body>
+<div id="cabecera">
+ <a href="../index.php">eLibrer&iacute;a aulaClic</a>
+  <ul>
+   <li>Libros Nuevos <img src="../imagenes/nuevos.gif" alt="Nuevos" width="21" height="27" /> </li>
+   <li>Libros Usados <img src="../imagenes/usados.gif" alt="Usados" width="21" height="27" /></li>
+  </ul>
+</div>
+
+<div id="menu">
+  <ul>
+	<li><a href="#">Mostrar</a>
+    	<ul>
+           <li><a href="nuevos.php">Nuevos</a></li>
+           <li><a href="usados.php">Usados</a></li>
+        </ul>
+    </li>
+    <li><a href="../ofertas.php">Ofertas</a></li>
+    <li><a href="../catalogo.php">Cat&aacute;logo</a></li>
+	<li><a href="../criticas.php">Cr&iacute;ticas</a></li>
+	<li><a href="../buscar.php">Buscar:</a></li>
+  </ul>
+</div>
+
+<div class="contenedor">
+ <div id="anuncios">
+  <div>
+    <a href="../usados.php"><img src="../imagenes/anuncio_largo.gif" alt="-10% en libros usados" /></a>
+  </div>
+
+  <div>
+	<script type="text/javascript">    
+    <!--
+		google_ad_client = "pub-1875034419890743";
+		google_ad_width = 125;
+		google_ad_height = 125;
+		google_ad_format = "125x125_as_rimg";
+		google_cpa_choice = "CAAQz9vnzwEaCIUwxV0cHMEdKMO393M";
+    //-->   
+    </script>   
+    <script type="text/javascript" src="http://pagead2.googlesyndication.com/pagead/show_ads.js"></script>
+  </div>
+
+  <div>
+    <a href="../catalogo.php"><img src="../imagenes/anuncio_corto.gif" alt="Compra 3 libros y te los enviamos a casa" /></a>
+  </div>
+
+  <div>
+	<script type="text/javascript">   
+    <!--    
+		google_ad_client = "pub-1875034419890743";    
+		google_ad_width = 125;   
+		google_ad_height = 125;   
+		google_ad_format = "125x125_as_rimg";    
+		google_cpa_choice = "CAAQz9vnzwEaCIUwxV0cHMEdKMO393M";   
+    //-->  
+    </script>
+    
+    <script type="text/javascript" src="http://pagead2.googlesyndication.com/pagead/show_ads.js"></script>
+  </div>
+
+ </div> <!-- fin div "anuncios" -->
+
+ <div id="contenido">
+
+	<!-- TemplateBeginEditable name="contenido" -->contenido<!-- TemplateEndEditable -->
+
+ </div> <!-- fin div "contenido" -->
+</div>   <!-- fin div "contenedor" -->
+
+<div id="pie">
+
+Esto es un proyecto de aulaClic.es
+
+<br />
+
+Copyright &copy; aulaClic S.L. 2010 
+
+</div>
+</body>
+</html>
